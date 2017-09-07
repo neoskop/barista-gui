d933c905abd8596e94047844e9e985f1ebf4aa97
Mark Wecke <wecke@neoskop.de>
2017-09-07 11:41:00 +0200

    feat(interceptors): add logout on unauthorized in auth interceptor

--- src/barista/http-interceptors/auth.interceptor.ts
+++ src/barista/http-interceptors/auth.interceptor.ts
@@ -1,21 +1,34 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpHeaderResponse, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/do';
+import { DispatcherService } from '../services/dispatcher.service';
+import { LogoutAction } from '../login/login.actions';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  
+  constructor(protected dispatcher : DispatcherService) {
+  
+  }
   
   intercept(req : HttpRequest<any>, next : HttpHandler) : Observable<HttpEvent<any>> {
     const token = localStorage.getItem('Authorization');
     
     if(token) {
-      req.clone({
+      req = req.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`
         }
       })
     }
     
-    return next.handle(req);
+    return next.handle(req).do(event => {
+      if(event instanceof HttpHeaderResponse) {
+        if(event.status === 401) {
+          this.dispatcher.dispatch(new LogoutAction());
+        }
+      }
+    });
   }
 }
